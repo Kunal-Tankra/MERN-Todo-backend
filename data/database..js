cc19d55710d4c498ec681badb5eb8d96eea88795
Kunal Tankra <kunaltankra2002@gmail.com>
2024-01-31 15:56:07 +0530

    fixed engine

--- data/database..js
+++ data/database..js
@@ -4,8 +4,8 @@ import mongoose from "mongoose"
 export const connectDB = ()=>{
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "Todo"
-    }).then(()=>{
-        console.log("db connected")
+    }).then((c)=>{
+        console.log("db connected " + c.connection.host)
     }).catch(error=>{
         console.log(error)
     })
