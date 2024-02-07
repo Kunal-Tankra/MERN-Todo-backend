e2e9f93d73cfa912b5367f0524077004813cda8f
Kunal Tankra <kunaltankra2002@gmail.com>
2024-02-07 11:23:41 +0530

    change in sendCookie func

--- utils/features.js
+++ utils/features.js
@@ -8,7 +8,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     res.status(statusCode).cookie("token", token, {
         httpOnly: true,
         maxAge: 15 * 60 * 1000, //15 min
-        // sameSite: "none",
+        sameSite: "none",
         secure: true
     }).json({
         success: true,
