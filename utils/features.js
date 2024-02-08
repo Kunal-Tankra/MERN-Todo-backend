27ec73665a53fbfffd08dea0047d9ba512d41429
Kunal Tankra <kunaltankra2002@gmail.com>
2024-02-08 09:02:53 +0530

    changed sendCookie func to store token in browser

--- utils/features.js
+++ utils/features.js
@@ -8,8 +8,8 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     res.status(statusCode).cookie("token", token, {
         httpOnly: true,
         maxAge: 15 * 60 * 1000, //15 min
-        // sameSite: "none",
-        // secure: true
+        sameSite: "none",
+        secure: true
     }).json({
         success: true,
         message
